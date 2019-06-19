fcfb7f7ea6419fc78756f32aefc1bf15fd8926c2
Ryan Kustas <rkustas@OLSONSGC.local>
2019-06-19 16:21:36 -0600

    updated folders and queries

--- SELECT_QUERIES.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-	FROM da_weather.us_weather_anomalies
-	LIMIT 500
-SELECT DISTINCT station_name
-	FROM da_weather.us_weather_anomalies
-SELECT date_ymd,degrees_from_mean,station_name
-	FROM da_weather.us_weather_anomalies
-	LIMIT 1200
