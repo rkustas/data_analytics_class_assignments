3612fbe10ebd276b27c442a4999bb39c7840f714
Ryan Kustas <rkustas@OLSONSGC.local>
2019-06-19 16:19:22 -0600

    updated folders and queries

--- QUERIES/Group_by_queries.sql
+++ QUERIES/Group_by_queries.sql
@@ -3,7 +3,7 @@ from da_pronto.station
 Order by station_id desc
 limit 10
 
-select distinct decommission_date as date, name
+SELECT distinct decommission_date as date, name
 from da_pronto.station
 Order by decommission_date ASC
 limit 15
